87ecd5d75a6ae8012a9741b08610ea6446e0103c
3gstudent <15814408+3gstudent@users.noreply.github.com>
2021-04-22 13:53:57 -1000

    Add files via upload

---
+++ ntfsDump/ntfsDump/NTFS.h
@@ -0,0 +1,18 @@
+/*
+ * NTFS include files
+ * 
+ * Copyright(C) 2010 cyb70289 <cyb70289@gmail.com>
+ */
+
+#ifndef __NTFS_H_CYB70289
+#define	__NTFS_H_CYB70289
+
+#pragma pack(8)
+
+#include "NTFS_Common.h"
+#include "NTFS_FileRecord.h"
+#include "NTFS_Attribute.h"
+
+#pragma pack()
+
+#endif
